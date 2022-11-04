33e3c08aa2b6d809f8a100e70e6104c657ffc16c
Umer-Aziz <umars25271997@gmail.com>
2022-11-04 16:13:51 +0500

    navbar mobile added

--- src/components/Homehero.js
+++ src/components/Homehero.js
@@ -12,7 +12,7 @@ const Homehero = () => {
 
             <div className='grid gap-y-6 lg:gap-y-0 lg:flex sm:gap-x-6 lg:gap-x-6'>
               {/* main div */}
-              <div className="sm:max-w-xl shadow">
+              <div className="sm:max-w-xl xl:min-w-[38rem] shadow">
                 <div className="max-h-80">
                   <img
                     className="sm:min-h-[18rem] bg-cover object-cover"
@@ -78,7 +78,7 @@ const Homehero = () => {
               </div>
 
               {/* Treding / Latest Post  */}
-              <div className="sm:w-72 md:w-80 xl:w-96 hidden sm:block">
+              <div className="sm:w-72 md:w-80 xl:w-88 hidden sm:block">
                 <div className="grid grid-cols-2">
                   <button onClick={()=>{setShow(true)}} className={`text-md font-mono py-1 border rounded-l hover:border-orange-600 hover:text-orange-600 ${show === true ? 'text-orange-600 border-orange-600' : 'text-gray-500 border-gray-600 border-opacity-50' }`}>
                     TRENDING
@@ -91,7 +91,7 @@ const Homehero = () => {
                 {/* Trending Block  */}
                {show === true ? <div className="my-8 trending grid gap-y-6">
                   <div className="flex gap-x-3">
-                    <div className="w-24 h-20 xl:w-30 xl:h-22 overflow-hidden">
+                    <div className="w-24 h-20 xl:w-28 xl:h-22 rounded-md overflow-hidden">
                       <img
                         className="bg-cover object-cover"
                         src="https://media.istockphoto.com/photos/group-of-business-people-in-office-cafeteria-picture-id1125784640?k=20&m=1125784640&s=612x612&w=0&h=zfl8ueIYN0zJi-3TETjb7kUhpa8jsyiAKFIymir1P8Q="
@@ -115,7 +115,7 @@ const Homehero = () => {
                   </div>
 
                   <div className="flex gap-x-3">
-                    <div className="w-24 h-20 xl:w-30 xl:h-22 overflow-hidden">
+                    <div className="w-24 h-20 xl:w-28 xl:h-22 rounded-md overflow-hidden">
                       <img
                         className="bg-cover object-cover"
                         src="https://media.istockphoto.com/photos/high-angle-view-asian-colleague-with-indian-coworker-in-wheelchair-a-picture-id1331469785?k=20&m=1331469785&s=612x612&w=0&h=EOXTjSxma3XTCNaEmG5geDfrLi-UTM18i8Ou5pt5Mf4="
@@ -137,7 +137,7 @@ const Homehero = () => {
                   </div>
 
                   <div className="flex gap-x-3">
-                    <div className="w-24 h-20 xl:w-30 xl:h-22 overflow-hidden">
+                    <div className="w-24 h-20 xl:w-28 xl:h-22 rounded-md overflow-hidden">
                       <img
                         className="bg-cover object-cover"
                         src="https://media.istockphoto.com/photos/running-on-treadmill-picture-id542197916?k=20&m=542197916&s=612x612&w=0&h=UFLdzG2sz7oX_YKbCaGV6_hXIGF_e7Lzxuw_E5EL3M4="
@@ -159,7 +159,7 @@ const Homehero = () => {
                   </div>
 
                   <div className="flex gap-x-3">
-                    <div className="w-24 h-20 xl:w-30 xl:h-22 overflow-hidden">
+                    <div className="w-24 h-20 xl:w-28 xl:h-22 rounded-md overflow-hidden">
                       <img
                         className="bg-cover object-cover"
                         src="https://media.istockphoto.com/photos/children-playing-soccer-outdoors-picture-id1321901975?k=20&m=1321901975&s=612x612&w=0&h=Xd0lpN7APrqaT369NBfIYgQ7Hs9BpWNJ2Qk9sbZO8Y0="
@@ -182,7 +182,7 @@ const Homehero = () => {
                 </div>:
                 <div className="my-8 trending grid gap-y-6">
                   <div className="flex gap-x-3">
-                    <div className="w-24 h-20 xl:w-30 xl:h-22 overflow-hidden">
+                    <div className="w-24 h-20 xl:w-28 xl:h-22 rounded-md overflow-hidden">
                       <img
                         className="bg-cover object-cover"
                         src="https://media.istockphoto.com/photos/group-of-business-people-in-office-cafeteria-picture-id1125784640?k=20&m=1125784640&s=612x612&w=0&h=zfl8ueIYN0zJi-3TETjb7kUhpa8jsyiAKFIymir1P8Q="
@@ -206,7 +206,7 @@ const Homehero = () => {
                   </div>
 
                   <div className="flex gap-x-3">
-                    <div className="w-24 h-20 xl:w-30 xl:h-22 overflow-hidden">
+                    <div className="w-24 h-20 xl:w-28 xl:h-22 rounded-md overflow-hidden">
                       <img
                         className="bg-cover object-cover"
                         src="https://media.istockphoto.com/photos/high-angle-view-asian-colleague-with-indian-coworker-in-wheelchair-a-picture-id1331469785?k=20&m=1331469785&s=612x612&w=0&h=EOXTjSxma3XTCNaEmG5geDfrLi-UTM18i8Ou5pt5Mf4="
