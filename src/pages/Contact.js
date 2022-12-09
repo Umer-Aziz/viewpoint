ccebf9e8d7174a8cf29494fb8272a514acadb9d7
Umer-Aziz <umars25271997@gmail.com>
2022-12-09 12:30:40 +0500

    contact form updated

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -54,22 +54,22 @@ const Contact = () => {
     </div>
 
     <div className="flex flex-col justify-center w-full lg:w-1/2">
-        <form>
+        <form action={process.env.REACT_APP_contact_form} method='POST'>
             <div className="-mx-2 md:items-center md:flex">
                 <div className="flex-1 px-2">
                     <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Full Name</label>
-                    <input type="text" placeholder="John Doe" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-gray-50 border border-gray-200 rounded-md dark:placeholder-gray-600  dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    <input type="text" placeholder="John Doe" name='fullname' required className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-gray-50 border border-gray-200 rounded-md dark:placeholder-gray-600 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                 </div>
 
                 <div className="flex-1 px-2 mt-4 md:mt-0">
                     <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email address</label>
-                    <input type="email" placeholder="johndoe@example.com" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-gray-50 border border-gray-200 rounded-md dark:placeholder-gray-600  dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    <input type="email" placeholder="johndoe@example.com" name='email' required className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-gray-50 border border-gray-200 rounded-md dark:placeholder-gray-600 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                 </div>
             </div>
 
             <div className="w-full mt-4">
                 <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Message</label>
-                <textarea className="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-gray-50 border border-gray-200 rounded-md md:h-56 dark:placeholder-gray-600  dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Message"></textarea>
+                <textarea name='message' required className="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-gray-50 border border-gray-200 rounded-md md:h-56 dark:placeholder-gray-600 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Message"></textarea>
             </div>
 
             <button className="px-6 py-3 mt-6 text-sm font-medium tracking-wide text-orange-600 capitalize transition-colors duration-300 transform border border-orange-600 rounded-md hover:bg-orange-600 hover:text-gray-50 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
